1eef5e84f6c338a2abae647923062ab34e154abd
The Rubyist <49131888+ruby-zen@users.noreply.github.com>
2021-09-05 13:30:54 +0530

    Created and implemented Employee Service provider

--- src/main/java/com/spacesstars/employeemanager/services/EmployeeService.java
+++ src/main/java/com/spacesstars/employeemanager/services/EmployeeService.java
@@ -1,4 +1,25 @@
 package com.spacesstars.employeemanager.services;
 
+import com.spacesstars.employeemanager.models.Employee;
+import com.spacesstars.employeemanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    // `Autowired` annotation can be used on a setter to avoid the use of the `<property>` tag in XML file
+    // Autowired -> gives fine-grained control
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
 }
