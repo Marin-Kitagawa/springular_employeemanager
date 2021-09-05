6e8b5f8018df07f61a8e2baad962ad32a9141df8
The Rubyist <49131888+ruby-zen@users.noreply.github.com>
2021-09-05 13:40:42 +0530

    Added findEmployeeById and deleteEmployeeById methods

--- src/main/java/com/spacesstars/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/com/spacesstars/employeemanager/repository/EmployeeRepository.java
@@ -3,9 +3,14 @@ package com.spacesstars.employeemanager.repository;
 import com.spacesstars.employeemanager.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 // Syntax of JpaRepository interface -> JpaRepository<Class, Type_Of_Primary_Key>
 // The primary key in our case is Id which is of type Long
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional findEmployeeById(Long id);
     // Hold `Ctrl` key and then click on a something to get a detailed info on it
     
 }
