0a41c9cf88f9da64886053c1a92e143f3bbc53da
The Rubyist <49131888+ruby-zen@users.noreply.github.com>
2021-09-05 13:21:58 +0530

    Created Employee repository

---
+++ src/main/java/com/spacesstars/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.spacesstars.employeemanager.repository;
+
+import com.spacesstars.employeemanager.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Syntax of JpaRepository interface -> JpaRepository<Class, Type_Of_Primary_Key>
+// The primary key in our case is Id which is of type Long
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // Hold `Ctrl` key and then click on a something to get a detailed info on it
+    
+}
