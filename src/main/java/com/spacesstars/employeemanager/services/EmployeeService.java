54716169d9af6adfd87434dc9b501a1b61940db1
The Rubyist <49131888+ruby-zen@users.noreply.github.com>
2021-09-05 14:49:58 +0530

    Updated EmployeeService

--- src/main/java/com/spacesstars/employeemanager/services/EmployeeService.java
+++ src/main/java/com/spacesstars/employeemanager/services/EmployeeService.java
@@ -5,11 +5,13 @@ import com.spacesstars.employeemanager.models.Employee;
 import com.spacesstars.employeemanager.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
 
